ad7e897105ff3891eedace84d6d36376461b9e2e
mc <mcsx97@gmail.com>
2019-02-05 17:57:53 +0100

    Indirizzo Server ripristinato su localhost

--- TuringFXLibrary/src/core/TuringParameters.java
+++ TuringFXLibrary/src/core/TuringParameters.java
@@ -15,34 +15,34 @@ import java.nio.charset.StandardCharsets;
  */
 public class TuringParameters {
     /* valori del Server */
-    public static final String SERVER_ADDRESS = "192.168.1.33";
-    
-    public static final int TCP_SERVER_PORT = 1111;   
+    public static final String SERVER_ADDRESS = "localhost";
+
+    public static final int TCP_SERVER_PORT = 1111;
     public static final int RMI_SERVER_PORT = 1112;
     public static final int CHAT_PORT = 1113;
-    
+
     /* parametri di sicurezza */
     public static final String PASSWORD_HASH_ALGORITHM = "SHA-256";
-    
+
     /* valori di riferimento per la registrazione degli utenti */
     public static final int MIN_USERNAME_LENGTH = 4;
     public static final int MAX_USERNAME_LENGTH = 15;
     public static final int MIN_PASSWORD_LENGTH = 4;
     public static final int MAX_PASSWORD_LENGTH = 15;
-    
+
     /* parametri inerenti alla chat */
-    
+
     /**
-     * Da 230.0.0.0 a 232.0.0.0 sono liberi e si coprono circa 
-     * 30 milioni di indirizzi, dovrebbero bastare
+     * Da 230.0.0.0 a 232.0.0.0 sono liberi e si coprono circa 30 milioni di
+     * indirizzi, dovrebbero bastare
      */
     public static final String FIRST_MULTICAST_ADDRESS = "230.0.0.0";
     public static final int MAX_CHAT_MESSAGE_LENGTH = 100; /* bytes */
     public static final int CHAT_MAX_BUFFER_SIZE = MAX_CHAT_MESSAGE_LENGTH + MAX_USERNAME_LENGTH;
-    
+
     /* charset di sistema */
     public static final Charset DEFAULT_CHARSET = StandardCharsets.UTF_8;
-    
+
     /* parametri dei documenti */
     public static final int DOCUMENT_MAX_SECTIONS = 10;
     public static final int MAX_SECTION_LENGTH = 10 * 1000 * 1000; /* cca 10 MB, max 20 MB (dipende dalla codifica) */
@@ -50,10 +50,5 @@ public class TuringParameters {
     /* uso la cartella temporanea definita dalla JVM */
     public static final String FILE_PARENT_DIRECTORY = System.getProperty("java.io.tmpdir");
     public static final String FILE_DIRECTORY_NAME = "Turing";
-    
-    
-    
-   
-    
-    
+
 }
